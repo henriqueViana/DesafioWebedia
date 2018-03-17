a8e5c671c0a6ad943c9c7a20b55e527e4ebccb76
henriqueviana <henriquehvisa@gmail.com>
2018-03-17 12:54:53 -0300

    Define default css

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes/Routes';
 
+import './default.css';
+
 ReactDOM.render(<Routes />, document.getElementById('root'));
